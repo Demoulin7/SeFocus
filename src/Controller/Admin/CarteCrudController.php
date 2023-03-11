30240631b31734b6b792a2a7c67f482fb78dc915
Nathan1Waxin <nathan.waxin@etu.unilim.fr>
2023-03-11 16:13:45 +0100

    mise en place du configurefields

--- src/Controller/Admin/CarteCrudController.php
+++ src/Controller/Admin/CarteCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Carte;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 
 class CarteCrudController extends AbstractCrudController
 {
@@ -12,14 +15,13 @@ class CarteCrudController extends AbstractCrudController
         return Carte::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            NumberField::new('id', 'ID')->hideOnForm(),
+            TextField::new('contenu'),
+            BooleanField::new('estPublique', 'Visible'),
         ];
     }
-    */
 }

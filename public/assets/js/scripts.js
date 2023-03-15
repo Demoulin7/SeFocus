73107a00cf20920454ce5fbe59a44751a90bbd17
demoulin7 <bosonhiggs@outlook.fr>
2023-03-15 05:20:33 +0100

    ajout cookies pour le theme

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,13 +1,45 @@
+function initColorMode(){
+
+    let cookie = getCookie("color_mode");
+
+    let checkbox = document.getElementById("checkbox-input-theme");
+
+    if (cookie == "light"){
+        checkbox.checked = true;
+        document.documentElement.setAttribute("color-mode", "light");
+        localStorage.setItem("color-mode", "light");
+    }
+}
+
 function toggleColorMode(checkboxElem) {
     if (checkboxElem.checked) {
         document.documentElement.setAttribute("color-mode", "light"); // Sets the user's preference in local storage
+
+        document.cookie = 'color_mode=light; path=/';
 
         localStorage.setItem("color-mode", "light");
     } else {
         document.documentElement.setAttribute("color-mode", "dark"); // Sets the user's preference in local storage
 
+        document.cookie = 'color_mode=dark; path=/';
+
         localStorage.setItem("color-mode", "dark");
     }
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 
@@ -39,5 +71,6 @@ const navSlide = () => {
 
 //Execution
 window.addEventListener('load', function () {
+    initColorMode();
     navSlide();
 })
